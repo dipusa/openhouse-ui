0239a9460521eb74682e58450aa713d075f7ca31
Dipankar Samanta <dipankar.s@dataglen.com>
2020-02-14 14:56:02 +0530

    change backednd host ip

--- src/constants.js
+++ src/constants.js
@@ -8,6 +8,6 @@ export const API_STATUS = {
 
 
 
-export const BASE_URL = process.env.API_URL ? process.env.API_URL : 'http://127.0.0.1:8000/api';
+export const BASE_URL = process.env.API_URL ? process.env.API_URL : 'http://139.59.39.142/api';
 
 
